abfb368ee0ea37025e5cb1ec417ef6efc32d9209
Harish Narayanasamy <h.narayanasamy@lakeba.com>
2021-02-16 16:39:08 +1100

    commit change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# required libraries
+# required libraries for running 
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
